5992c9e48cd430232ef01937bf80806bc819a92b
Milena de Oliveira <milemoliveira@gmail.com>
2022-02-08 18:50:10 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Contador de pessoas
+
+Aplicativo desenvolvido com **Flutter**, ele realiza o controle de disponibilidade de lugares no estabelecimento de acordo com uma capacidade pré-estabelecida. 
+
+![](/lib/images/count2.png)
+![](/lib/images/count1.png)
+
+***
+## :computer: Sobre o projeto
+
+Aplicativo introdutório para apresentar os conceitos **básicos** do flutter. Ele possui um contador que pode aumentar e diminuir, até atingir a capacidade máxima. A qualquer momento é possível zerar o contador. 
+
+***
+
+## :bookmark_tabs: Funcionalidades
+:heavy_check_mark: Visualizar o número de pessoas no estabelecimento.
+:heavy_check_mark: Adicionar e remover pessoas.
+:heavy_check_mark: Informar quando houver lugares disponíveis ou estiver lotado.
+:heavy_check_mark: Permitir zerar o contator. 
+
+*** 
+
+
+
+
+
+
---
+++ lib/main.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int count = 0;
+
+  void decrement() {
+    setState(() {
+      count --;
+    });
+  }
+
+  void increment() {
+    setState(() {
+      count ++;
+    });
+  }
+
+  void zerar() {
+    setState(() {
+      count = 0;
+    });
+  }
+
+  bool get isEmpty => count == 0;
+  bool get isFull => count == 25;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: null, 
+        backgroundColor: const Color(0XFF141926),),
+      backgroundColor: const Color(0XFF1C2130),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(30),
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.width * 0.15,
+              decoration: BoxDecoration(
+                color: isFull? const Color(0X77D14334) : const Color(0X77B3E099),
+                borderRadius: BorderRadius.circular(6)
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(isFull ? 'Lotado!' : 'Disponível!',
+                    style: const TextStyle(
+                      color: Color(0XFFFFFFFF),
+                      fontSize: 25,
+                    ),),
+                ],
+              ),
+            ),
+          ),
+        const Text('TOTAL DE PESSOAS',
+          style: TextStyle(
+            fontSize: 30,
+            color: Color(0XFFFFEAAD)
+          ),),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Text(
+            count.toString(),
+            style: const TextStyle(
+              fontSize: 100,
+              color: Color(0XFFFFEAAD),
+            ),
+
+          ),
+        ),
+        const Text(
+          'Capacidade máxima: 25 pessoas',
+          style: TextStyle(
+            color: Color(0X50FFFFFF),
+            fontSize: 17,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(40),
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: const Color(0XFF028F76),
+                    fixedSize: const Size(120, 120),
+                    primary: const Color(0XFFFFFFFF),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)
+                    )
+                  ),
+                  onPressed: isFull ? null : increment, 
+                  child: const Text('ENTRADA',
+                  style: TextStyle(color: Colors.white,
+                  fontSize: 20),),),
+                const SizedBox(width: 70,),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: const Color(0XFFD14334), 
+                    fixedSize: const Size(120, 120),
+                    primary: const Color(0XFFb3a394),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)
+                    )
+                  ),
+                  onPressed: isEmpty ? null : decrement, 
+                  child: const Text('SAÍDA',
+                  style: TextStyle(color: Color(0XFFFFFFFF),
+                  fontSize: 20)))],
+            ),
+        ),
+        TextButton(
+          style: TextButton.styleFrom(
+            backgroundColor: const Color(0XFF474D61),
+            fixedSize: const Size(160, 40), 
+          ),
+          onPressed: zerar, 
+          child: const Text('Zerar contador', 
+            style: TextStyle(color: Color(0XFFFFFFFF),
+              fontSize: 18),))
+        ],
+      ),
+    );
+  }
+}
